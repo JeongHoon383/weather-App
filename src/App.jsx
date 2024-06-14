f7563825626040ed7d61696d407099acf74d4574
Jeong Hoon <gbg9609@naver.com>
2024-06-14 23:06:50 +0900

    get weather api

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+
+      const curWeatherData = async () => {
+        const res = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=c6de406e083c681266b9287653186f07`
+        );
+        const result = res.json();
+        return result;
+      };
+      curWeatherData().then((res) => setData(res));
+    });
+  }, []);
+
+  console.log(data);
+
+  if (!data) {
+    return <div>데이터 로딩중...</div>;
+  }
+
   return <div>weather App</div>;
 }
 
